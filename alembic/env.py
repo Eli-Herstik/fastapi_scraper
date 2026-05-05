2f97ca0951d4680b7168b3bb6473773e2dd8e8b8
Eli Herstik <a0548448256@gmail.com>
2026-05-05 21:48:17 +0300

    Fix SQLAlchemy URL escaping in Alembic configuration

--- alembic/env.py
+++ alembic/env.py
@@ -21,7 +21,7 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", _database_url())
+config.set_main_option("sqlalchemy.url", _database_url().replace("%", "%%"))
 target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
